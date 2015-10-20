836a8b2d9ff2dabcb110a3246900efdb6e950783
codio-units <spackham@codio.com>
2015-10-20 12:59:17 +0000

    Copied comments from Floed example to Javascript example

--- 3-string/string.js
+++ 3-string/string.js
@@ -1,10 +1,10 @@
 
-str = 'Hello'
-newStr = str + ' world!'
+str = 'Hello'               // Create a string variable
+newStr = str + ' world!'    // 'Hello world!'
 console.log(newStr)
 
-newStr = '123' + 10
+newStr = '123' + 10         // '12310'
 console.log(newStr)
 
-newStr  = '123' + '456'
+newStr  = '123' + '456'     // '123456'
 console.log(newStr)
