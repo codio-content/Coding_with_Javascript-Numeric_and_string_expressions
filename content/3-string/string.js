61f4360ff606ee41aec0f2ef07f6df9d28cecc86
codio-units <spackham@codio.com>
2015-10-30 12:29:51 +0000

    Made a few reorganization changes moving the strings section in from the of the error fix section. It loooks like that is how it was in the first place. I can see why someone moved it, but since it has string errors to fix, we have to cover strings first. Also added some comments to a code example.

--- content/3-string/string.js
+++ content/3-string/string.js
@@ -1,10 +1,10 @@
 
-var str = 'Hello'               // Create a string variable
-var newStr = str + ' world!'    // 'Hello world!'
-console.log(newStr)
+var str = 'Hello'             // Create a string variable
+var newStr = str + ' world!'  // 'Hello world!'
+console.log(newStr)           // output
 
-newStr = '123' + 10         // '12310'
-console.log(newStr)
+newStr = '123' + 10           // '12310'
+console.log(newStr)           // output
 
-newStr  = '123' + '456'     // '123456'
-console.log(newStr)
+newStr  = '123' + '456'       // '123456'
+console.log(newStr)           // output
